d751232c49660195ee73354253394f95daac12a0
Laura Trager <laura.trager@gmail.com>
2017-10-02 15:26:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,5 @@
-# code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(array, index)
+  if array[index] == " " #|| array[index] == " " || array[nil]
+end
