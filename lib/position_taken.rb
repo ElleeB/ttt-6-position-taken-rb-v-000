4ebad40e5d617ce769ec5fff948facf24d812d33
Laura Trager <laura.trager@gmail.com>
2017-10-02 15:37:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(array, index)
-  #if array[index] == " " || array[index] == " " || array[nil]
-  #  false
-  #end
+  if array[index] == "  " || array[index] == " " || array[nil]
+    false
+  end
 end
